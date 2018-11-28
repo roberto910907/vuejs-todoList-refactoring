e0373d7746b5be292033b8117e63e4460725cc69
Roberto Rielo <roberto910907@gmail.com>
2018-11-28 12:13:55 -0500

    Adding new html base structure

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,85 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="columns">
+            <div class="column is-one-third main-task">
+                <nav class="panel has-background-light">
+                    <p class="panel-heading has-background-dark has-text-white">
+                        Task
+                    </p>
+                    <div class="panel-block">
+                        <p class="control has-icons-left">
+                            <input class="input is-small" type="text" placeholder="New Task">
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-search" aria-hidden="true"></i>
+                            </span>
+                        </p>
+                    </div>
+                    <a class="panel-block is-active" style="background-color: #f5f5f5;">
+                        <input type="checkbox">
+                        Foo
+                    </a>
+                    <a class="panel-block">
+                        <input type="checkbox">
+                        Bar
+                    </a>
+                    <a class="panel-block">
+                        <input type="checkbox">
+                        Baz
+                    </a>
+                    <label class="panel-block">
+                        <input type="checkbox">
+                        Other
+                    </label>
+                </nav>
+            </div>
+            <div class="column detail-task">
+                <nav class="panel has-background-light">
+                    <p class="panel-heading has-background-dark has-text-white">
+                        Foo Detail
+                    </p>
+                    <div class="panel-block">
+                        <p class="control has-icons-left">
+                            <input class="input is-small" type="text" placeholder="Tittle">
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-search" aria-hidden="true"></i>
+                            </span>
+                        </p>
+                    </div>
+                    <a class="panel-block">
+                        <textarea class="textarea" placeholder="Other Details"></textarea>
+                    </a>
+                    <label class="panel-block">
+                        <nav class="panel is-spaced" style="width: 100%">
+                            <p class="panel-heading has-background-grey has-text-white">
+                                Sub-Tasks
+                            </p>
+                            <a class="panel-block">
+                                <input type="checkbox">
+                                Bar
+                            </a>
+                            <a class="panel-block">
+                                <input type="checkbox">
+                                Baz
+                            </a>
+                            <label class="panel-block">
+                                <input type="checkbox">
+                                Other
+                            </label>
+                        </nav>
+                    </label>
+                </nav>
+            </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
